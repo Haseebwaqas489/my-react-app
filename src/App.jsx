c6644f01a81feb52470efd1edcbbcb8041656648
Haseebwaqas489 <waqashaseeb57@gmail.com>
2026-08-20 16:41:43 +0500

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-
-
-
 function App() {
   const structuredData = {
     "@context": "https://schema.org",
     "@type": "Product",
     "name": "Laptop",
-    "model":"elitebook",
-    "price":"89000",
-    "color":"white"
+    "model": "elitebook",
+    "price": "89000",
+    "color": "white"
   };
 
   return (
     <>
       <h1>Laptop</h1>
-      <p>Model:{structuredData.model}</p>
-      <p>Price:{structuredData.price}</p>
-      <p>Color:{structuredData.color}</p>
+      <p>Model: {structuredData.model}</p>
+      <p>Price: {structuredData.price}</p>
+      <p>Color: {structuredData.color}</p>
 
       <script
         type="application/ld+json"
